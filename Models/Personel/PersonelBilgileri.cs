4d4a58e26a31ab389222f398e43ce0187199ef90
55279283562 <sahapliogluzehra@gmail.com>
2023-12-26 23:01:42 +0300

    Proje dosyası ekle.

---
+++ Models/Personel/PersonelBilgileri.cs
@@ -0,0 +1,87 @@
+﻿using PROJETAKIP.Models.ProjeTakip;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PROJETAKIP.Models.Personel
+{
+    public class PersonelBilgileri
+    {
+        public PersonelBilgileri()  // many-to-many ilişkisi için gerekli
+        {
+            this.PersonelProjeleris = new HashSet<PersonelProjeleri>();
+        }
+
+
+        [Key]
+        public int PersonelBilgileriId { get; set; }
+
+        [DisplayName("E-Posta Adresi")]  //label kısmında bu gözükür
+        public string Eposta { get; set; }
+
+        [DisplayName("Şifre")]
+        [StringLength(15, ErrorMessage = "Max uzunluk 15 karakterden fazla olamaz!")]
+        public string Sifre { get; set; }
+
+        [DisplayName("Yetki")]
+        [StringLength(25, ErrorMessage = "Max uzunluk 25 karakterden fazla olamaz!")]
+        public string Yetki { get; set; }
+
+        [DisplayName("Ad Soyad")]
+        [StringLength(50, ErrorMessage = "Max uzunluk 50 karakterden fazla olamaz!")]
+        public string AdSoyad { get; set; }
+
+        [DisplayName("TC Kimlik Numarası")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "Geçerli bir TC Kimlik Numarası giriniz.")]
+        public string TCNO { get; set; }
+
+        [DisplayName("Departman")]
+        [StringLength(25, ErrorMessage = "Max uzunluk 25 karakterden fazla olamaz!")]
+        public string Departman { get; set; }
+
+        [DisplayName("Görev")]
+        [StringLength(25, ErrorMessage = "Max uzunluk 25 karakterden fazla olamaz!")]
+        public string Gorev { get; set; }
+
+        [DisplayName("Açıklama")]
+        public string PozisyonAciklama { get; set; }
+
+        [DisplayName("Telefon Numarası")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "Geçerli bir telefon numarası giriniz.")]
+        public string TelNo { get; set; }
+
+        [DisplayName("Adres Bilgisi")]
+        public string Adres { get; set; }
+
+        [DisplayName("Medeni Hali")]
+        [StringLength(15, ErrorMessage = "Max uzunluk 15 karakterden fazla olamaz!")]
+        public string MedeniHal { get; set; }
+
+        [DisplayName("Yakın Bilgisi")]
+        [StringLength(25, ErrorMessage = "Max uzunluk 25 karakterden fazla olamaz!")]
+        public string YakinBilgisi { get; set; }
+
+        [DisplayName("Yakın TC Kimlik Numarası")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "Geçerli bir TC Kimlik Numarası giriniz.")]
+        public string YakinTC { get; set; }
+
+        [DisplayName("Yakın Ad Soyad")]
+        [StringLength(50, ErrorMessage = "Max uzunluk 50 karakterden fazla olamaz!")]
+        public string YakinAdSoyad { get; set; }
+
+        [DisplayName("Yakın Telefon Numarası")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "Geçerli bir telefon numarası giriniz.")]
+        public string YakinTel { get; set; }
+
+        [DisplayName("Doğum Tarihi")]
+        public DateTime DogumTarihi { get; set; }
+
+        [DisplayName("İşe Giriş Tarihi")]
+        public DateTime? IseGirisTarihi { get; set; }
+
+        public virtual ICollection<PersonelProjeleri> PersonelProjeleris { get; set; }  //many-to-many ilişkisi için
+    }
+}
---
+++ Models/ProjeTakip/PersonelProjeleri.cs
@@ -0,0 +1,47 @@
+﻿using PROJETAKIP.Models.Personel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PROJETAKIP.Models.ProjeTakip
+{
+    public class PersonelProjeleri
+    {
+        public PersonelProjeleri() 
+        {
+            this.PersonelBilgileris = new HashSet<PersonelBilgileri>();
+        }
+
+
+        [Key]
+        public int PersonelProjeId { get; set; }
+
+        [DisplayName("Proje Başlığı")]
+        [StringLength(50, ErrorMessage = "Max uzunluk 50 karakterden fazla olamaz!")]
+        public string ProjeBaslik {  get; set; }
+
+        [DisplayName("Proje Açıklaması")]
+        public string ProjeAciklama { get; set; }
+
+        [DisplayName("Proje Oluşturma Tarihi")]
+        public DateTime OlusturmaTarihi { get; set; }
+
+        [DisplayName("Öncelik Durumu")]
+        [StringLength(25, ErrorMessage = "Max uzunluk 25 karakterden fazla olamaz!")]
+        public string OncelikDurumu { get; set; }
+
+        [DisplayName("Proje Tamamlanma Oranı")]
+        public int  TamamlanmaOrani { get; set; }
+
+        [DisplayName("Projenin Tamamlanma Tarihi")]
+        public DateTime? TamamlanmaTarihi { get; set; }
+
+        [DisplayName("Projenin Tamamlanma Durumu")]
+        public bool TamamlanmaDurumu { get; set; }
+
+        public virtual ICollection<PersonelBilgileri> PersonelBilgileris { get; set; }
+    }
+}
