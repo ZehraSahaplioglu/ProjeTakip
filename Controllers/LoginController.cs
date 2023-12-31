40e8770b66ebb1f71b0492c67f658ef10fef0267
55279283562 <sahapliogluzehra@gmail.com>
2023-12-31 20:17:34 +0300

    Projeye Login kısmı eklendi

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using PROJETAKIP.Models.DataContext;
+using PROJETAKIP.Models.Personel;
+
+namespace PROJETAKIP.Controllers
+{
+    public class LoginController : Controller
+    {
+        private ProjeTakipDBContext db = new ProjeTakipDBContext();
+
+
+        //Login kısmı:
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(PersonelBilgileri admin)
+        {
+            var bilgiler = db.PersonelBilgileris.FirstOrDefault(x => x.AdSoyad==admin.AdSoyad && x.Sifre==admin.Sifre);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.AdSoyad, false);
+                Session["Kullanici"] = bilgiler.AdSoyad.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+ 
+        }
+
+
+        // Logout kısmı:
+
+        public ActionResult Logout()
+        {
+            Session["Kullanici"] = null;
+            Session.Abandon();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
