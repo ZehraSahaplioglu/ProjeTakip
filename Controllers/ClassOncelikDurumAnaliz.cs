0f5f987a5b7dbb7659f02513d45ca20cc95a455d
55279283562 <sahapliogluzehra@gmail.com>
2023-12-31 16:38:35 +0300

    Tamamlanmış Projeler içim Raporlamalar yapıldı

---
+++ Controllers/ClassOncelikDurumAnaliz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PROJETAKIP.Controllers
+{
+    public class ClassOncelikDurumAnaliz
+    {
+        // grafikde kullanmak için
+        public string onceliktipi {  get; set; }
+        public int oncelikadeti { get; set; }
+    }
+}
---
+++ Controllers/ProjeRaporlariController.cs
@@ -0,0 +1,38 @@
+﻿using PROJETAKIP.Models.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PROJETAKIP.Controllers
+{
+    public class ProjeRaporlariController : Controller
+    {
+        public ProjeTakipDBContext db = new ProjeTakipDBContext();
+
+        public ActionResult TamamlanmisOncelikGruplari()
+        {
+            return View();
+        }
+
+        public ActionResult VisualizeTamamlanmisDurumGruplari()
+        {
+            return Json(OncelikGrupTipi(), JsonRequestBehavior.AllowGet);  //grafik döndüreceği için json kullandık
+        }
+
+        //grafik oluşturacağımız class:
+        public List<ClassOncelikDurumAnaliz> OncelikGrupTipi()
+        {
+            ;
+            List<ClassOncelikDurumAnaliz> snf = new List<ClassOncelikDurumAnaliz>();
+            using (var c = new ProjeTakipDBContext())
+                snf = c.PersonelProjeleris.Where(x => x.TamamlanmaDurumu == true).GroupBy(p => p.OncelikDurumu).Select(x => new ClassOncelikDurumAnaliz
+                {
+                    onceliktipi = x.Key,
+                    oncelikadeti = x.Count(),
+                }).ToList();
+            return snf;
+        }
+    }
+}
